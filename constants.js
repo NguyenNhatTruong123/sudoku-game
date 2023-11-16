3667cd79991c4ed681a581c9c25bb4fa23fb4e45
Truongnn <truongnn@allexceed.co.jp>
2023-11-16 10:17:02 +0700

    Refactor code and fix bug

---
+++ constants.js
@@ -0,0 +1,18 @@
+var maximumError = {
+    "easy": 2,
+    "medium": 2,
+    "hard": 3,
+    "very-hard": 4,
+    "insane": 5,
+    "inhuman": 10
+}
+
+var cellColorBorder = "3px solid green"
+
+var clickCellColor = "goldenrod"
+
+var inputCellErrorColor = "#ff00007a"
+
+var normalCellColor = "white"
+
+var activeCellColor = "beige"
--- scripts.js
+++ scripts.js
@@ -19,15 +19,6 @@ var activeCol = 0
 var isNote = false
 var errorCount = 0
 
-var maximumError = {
-    "easy": 2,
-    "medium": 2,
-    "hard": 3,
-    "very-hard": 4,
-    "insane": 5,
-    "inhuman": 10
-}
-
 tutorial.innerText = " Dùng chuột để chọn vào ô cần nhập số, hoặc dùng các phím di chuyển để di chuyển ô đang chọn.\nSử dụng các phím số từ 1-9 để nhập.\nSử dụng phím 0 để bật/tắt ghi chú."
 
 function setUpBoard() {
@@ -65,7 +56,7 @@ function enableNote() {
     if (isNote) {
         note.style.backgroundColor = "green"
     } else {
-        note.style.backgroundColor = "white"
+        note.style.backgroundColor = normalCellColor
     }
 }
 
@@ -73,25 +64,23 @@ function createTdElement(i, j) {
     let td = document.createElement("td");
     td.id = i + "&" + j
 
-    let colorBorder = "3px solid green"
-
     if (i === 0 || i === 3 || i === 6) {
-        td.style.borderTop = colorBorder
+        td.style.borderTop = cellColorBorder
     }
     if (i === 2 || i === 5 || i === 8) {
-        td.style.borderBottom = colorBorder
+        td.style.borderBottom = cellColorBorder
     }
 
     if (j === 0 || j === 3 || j === 6) {
-        td.style.borderLeft = colorBorder
+        td.style.borderLeft = cellColorBorder
     }
     if (j === 2 || j === 5 || j === 8) {
-        td.style.borderRight = colorBorder
+        td.style.borderRight = cellColorBorder
     }
 
     td.setAttribute('onclick', "clickCell(" + i + "," + j + ")");
     if (Board[i][j].value !== ".") {
-        td.style.color = "goldenrod"
+        td.style.color = clickCellColor
         td.innerText = Board[i][j].value
     }
     return td;
@@ -100,8 +89,8 @@ function createTdElement(i, j) {
 
 function clickCell(i, j) {
     if (!Board[i][j].canChangedValue) return
-    if (activeCell.style.backgroundColor === "white" || activeCell.style.backgroundColor === "beige") {
-        activeCell.style.backgroundColor = "white"
+    if (activeCell.style.backgroundColor === normalCellColor || activeCell.style.backgroundColor === activeCellColor) {
+        activeCell.style.backgroundColor = normalCellColor
     }
     activeRow = i
     activeCol = j
@@ -120,17 +109,15 @@ window.addEventListener('keydown', (e) => {
             }
         } else {
             activeCell.style.fontSize = "xxx-large"
-            if (activeCell && Board[activeRow][activeCol].value === ".") {
-                activeCell.innerText = num.toString()
-                Board[activeRow][activeCol].value = num.toString()
-                if (errorCheck === "check") {
-                    if (Board[activeRow][activeCol].value !== SolveBoard[activeRow][activeCol]) {
-                        activeCell.style.backgroundColor = "#ff00007a"
-                        errorCount++;
-                        errorCheckStatus.innerText = "Total error: " + errorCount + "/" + maximumError[difficulty]
-                    } else {
-                        activeCell.style.backgroundColor = "white"
-                    }
+            activeCell.innerText = num.toString()
+            Board[activeRow][activeCol].value = num.toString()
+            if (errorCheck === "check") {
+                if (Board[activeRow][activeCol].value !== SolveBoard[activeRow][activeCol]) {
+                    activeCell.style.backgroundColor = inputCellErrorColor
+                    errorCount++;
+                    errorCheckStatus.innerText = "Total error: " + errorCount + "/" + maximumError[difficulty]
+                } else {
+                    activeCell.style.backgroundColor = activeCellColor
                 }
             }
         }
@@ -176,17 +163,17 @@ window.addEventListener('keydown', (e) => {
 function applyActiveCell() {
     activeCell = document.getElementById(activeRow + "&" + activeCol)
     if (Board[activeRow][activeCol].value !== "." && Board[activeRow][activeCol].value !== SolveBoard[activeRow][activeCol]) {
-        activeCell.style.backgroundColor = "#ff00007a"
+        activeCell.style.backgroundColor = inputCellErrorColor
     } else {
-        activeCell.style.backgroundColor = "beige"
+        activeCell.style.backgroundColor = activeCellColor
     }
 }
 
 function checkRightNumber() {
     if (Board[activeRow][activeCol].value !== "." && Board[activeRow][activeCol].value !== SolveBoard[activeRow][activeCol]) {
-        activeCell.style.backgroundColor = "#ff00007a"
+        activeCell.style.backgroundColor = inputCellErrorColor
     } else {
-        activeCell.style.backgroundColor = "white"
+        activeCell.style.backgroundColor = normalCellColor
     }
 }
 
